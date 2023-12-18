d9402c47280ea933cb13bce43fa64734efa12795
oGabrielFreitas <ogabrielfreitas@gmail.com>
2023-12-17 21:22:09 -0300

    Rota de files ativada

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express'
 // import { userRouter } from './user.routes'
 // import { sessionsRouter } from './sessions.routes'
-// import { filesRouter } from './files.routes'
+import { filesRouter } from './files.routes'
 
 const routes = Router()
 
 // routes.use('/users', userRouter)
 // routes.use('/sessions', sessionsRouter)
-// routes.use('/files', filesRouter)
+routes.use('/files', filesRouter)
 
 routes.get('/', (req, res) => {
   return res.status(200).json({ message: 'Tudo Ok' })
