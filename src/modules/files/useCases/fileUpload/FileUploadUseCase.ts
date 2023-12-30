6aa0c2c5a6f4d88d85fab71466132802900dd397
oGabrielFreitas <ogabrielfreitas@gmail.com>
2023-12-30 00:13:23 -0300

    testando memfs

--- src/modules/files/useCases/fileUpload/FileUploadUseCase.ts
+++ src/modules/files/useCases/fileUpload/FileUploadUseCase.ts
@@ -16,33 +16,33 @@ interface UploadResponse {
 
 class FileUploadUseCase {
   async execute({ userId, file }: FileUpload): Promise<UploadResponse> {
-    // // Transforma o Buffer do arquivo em Blob
-    // const fileBlob = new Blob([file.buffer], { type: 'application/pdf' })
+    // Transforma o Buffer do arquivo em Blob
+    const fileBlob = new Blob([file.buffer], { type: 'application/pdf' })
 
-    // // Load no arquivo, com PDFLoader
-    // const loader = new PDFLoader(fileBlob)
-    // const docs = await loader.load()
+    // Load no arquivo, com PDFLoader
+    const loader = new PDFLoader(fileBlob)
+    const docs = await loader.load()
 
-    // // try {
-    // // Salva o buffer do arquivo no bando de dados como binário.
-    // const savedFile = await MyPrismaClient.uplodadedFile.create({
-    //   data: {
-    //     userOwnerId: userId, // ID do usuário que possui o arquivo
-    //     fileName: file.originalname, // Nome original do arquivo
-    //     data: file.buffer, // Salvando o buffer no banco de dados
-    //   },
-    // })
+    // try {
+    // Salva o buffer do arquivo no bando de dados como binário.
+    const savedFile = await MyPrismaClient.uplodadedFile.create({
+      data: {
+        userOwnerId: userId, // ID do usuário que possui o arquivo
+        fileName: file.originalname, // Nome original do arquivo
+        data: file.buffer, // Salvando o buffer no banco de dados
+      },
+    })
 
-    // // Chama a função de vector store do FAISS STORE
-    // const vectorStoreService = new VectorStoreDocumentService()
-    // const directory = './teste'
-    // await vectorStoreService.save({ docs, fileOwnerId: savedFile.id })
-    // // await vectorStoreService.save({ docs, fileOwnerId: 'lazarento' })
+    // Chama a função de vector store do FAISS STORE
+    const vectorStoreService = new VectorStoreDocumentService()
+    const directory = './teste'
+    await vectorStoreService.save({ docs, fileOwnerId: savedFile.id })
+    // await vectorStoreService.save({ docs, fileOwnerId: 'lazarento' })
 
-    // //
-    // // } catch (err) {
-    // //   throw new Error('Database file saving error!')
-    // // }
+    //
+    // } catch (err) {
+    //   throw new Error('Database file saving error!')
+    // }
 
     return { message: 'Arquivo processado com sucesso!' }
   }
